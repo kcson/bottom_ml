c6c2a24a1da4f048504d08313f4af67e630c2ff5
ProDesk <fob330@gmail.com>
2019-09-30 18:21:21 +0900

    commit

--- ch03.py
+++ ch03.py
@@ -10,8 +10,21 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+def relu(x):
+    return np.maximum(0, x)
+
+
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = relu(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
+
+A = np.array([1, 2, 3, 4])
+print(A)
+print(np.ndim(A))
+print(A.shape)
+
+B = np.array([[1, 2], [3, 4], [5, 6]])
+C = np.array([7, 8])
+print(np.dot(B, C))
