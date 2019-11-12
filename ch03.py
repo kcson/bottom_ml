f1afe261ae8588d3fd60a1b71bec90594ebe7500
ProDesk <fob330@gmail.com>
2019-11-12 15:17:27 +0900

    commit

--- ch03.py
+++ ch03.py
@@ -1,3 +1,5 @@
+import pickle
+
 from dataset.mnist import load_mnist
 from PIL import Image
 
@@ -62,17 +64,55 @@ def img_show(img):
     pil_img.show()
 
 
-(x_train, t_train), (x_test, t_text) = load_mnist(flatten=True, normalize=False)
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
 
-img = x_train[0]
-label = t_train[0]
-print(label)
 
-print(img.shape)
-img = img.reshape(28,28)
-print(img.shape)
+def init_network():
+    with open("dataset/sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
 
-img_show(img)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print(float(accuracy_cnt) / len(x))
+
+# (x_train, t_train), (x_test, t_text) = load_mnist(flatten=True, normalize=False)
+#
+# img = x_train[0]
+# label = t_train[0]
+# print(label)
+#
+# print(img.shape)
+# img = img.reshape(28, 28)
+# print(img.shape)
+#
+# img_show(img)
 # a = np.array([0.3, 2.9, 4.0])
 # s = softmax(a)
 # print(s)
