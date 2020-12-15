ea2d2c9acb6da2b96014581abfb7b7c597e3f6e8
kcson <fob330@gmail.com>
2020-12-15 22:00:01 +0900

    commit

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -6,13 +6,17 @@ from matplotlib import pyplot as plt
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
-iters_num = 100
+iters_num = 1
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
 
-network = TwoLayerNet(input_size=784,hidden_size=50,output_size=10)
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
@@ -26,6 +30,13 @@ for i in range(iters_num):
 
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train_acc test_acc | " + str(train_acc) + " , " + str(test_acc))
 
 plt.plot(train_loss_list)
 plt.show()
