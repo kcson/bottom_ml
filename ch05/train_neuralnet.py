950ad8686a091d5ed9847b64e7c572848bb9d0c1
angryturtle <tom@last30min.com>
2020-12-18 13:35:40 +0900

    commit

--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -1,6 +1,6 @@
 import numpy as np
 from dataset.mnist import load_mnist
-from ch05.ch05_2 import TwoLayerNet
+from ch05.tow_layer_net import TwoLayerNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
